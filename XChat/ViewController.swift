9c9f1c3e0cf6eef721dc0d75b18f0fc92d86f5bc
Kamal <kamal@kamals-macbook-pro.local>
2019-04-11 21:08:53 +0300

    Initial Commit

---
+++ XChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XChat
+//
+//  Created by Kamal on 1/22/1398 AP.
+//  Copyright © 1398 Kamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
